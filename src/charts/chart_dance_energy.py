b690e310e2dab28e6070d10feb9e6f1b13c915d1
jaybrandon <jaybrandon@outlook.com>
2025-05-22 13:26:27 +0200

    feat: improve dance/energy scatter readability

--- src/charts/chart_dance_energy.py
+++ src/charts/chart_dance_energy.py
@@ -1,18 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from src.data_loader import get_hit_threshold
 
 
 def plot_dance_vs_energy(df: pd.DataFrame):
-    hit_threshold = get_hit_threshold(df)
-    df_pop = df[df['track_popularity'] >= hit_threshold]
-    df_upop = df[df['track_popularity'] < hit_threshold]
-
-    size = 0.5
     f, ax = plt.subplots()
-    ax.scatter(df_upop['danceability'], df_upop['energy'], s=size)
-    ax.scatter(df_pop['danceability'], df_pop['energy'], s=size)
-    ax.set_title('Danceability vs Energy')
+
+    sns.scatterplot(
+        data=df[df["is_hit"] == False],
+        x="danceability",
+        y="energy",
+        color="blue",
+        alpha=0.25,
+        s=12,
+        edgecolor=None,
+        ax=ax,
+        label='False'
+    )
+
+    sns.scatterplot(
+        data=df[df["is_hit"] == True],
+        x="danceability",
+        y="energy",
+        color="orange",
+        alpha=0.6,
+        s=14,
+        edgecolor=None,
+        ax=ax,
+        label="True"
+    )
+    plt.legend(title="Hit Song")
     ax.set_xlabel('Danceability')
     ax.set_ylabel('Energy')
 
